4de506683a076c48bf3ccd46590bdbef98f353d0
Peter Widmer <peter.widmer@mail.com>
2023-11-11 11:25:29 +0100

    Improved config files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,22 @@
 // rollup.config.js
 import typescript from '@rollup/plugin-typescript';
+import dts from "rollup-plugin-dts";
 
-export default {
-  input: 'src/form-store.ts',
-  output: {
-    dir: 'dist',
-    format: 'cjs'
+export default [
+  {
+	  input: 'src/form-store.ts',
+	  output: {
+		dir: 'dist',
+		format: 'cjs'
+	  },
+	  plugins: [typescript()]
   },
-  plugins: [typescript()]
-};
+  {
+	  input: 'dist/dts/form-store.d.ts',
+	  output: {
+		file: 'dist/form-store.d.ts',
+		format: 'es'
+	  },
+	  plugins: [dts()]
+  }
+];
